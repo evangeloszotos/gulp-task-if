79454b6b22c13903816dae58e164fad941be9c5b
Evangelos Zotos <evangelos.zotos@googlemail.com>
2019-03-12 20:00:22 +0100

    adds unit testing

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ function emptyGulpTask(done) {
 }
 
 function gulpTaskIf(condition, ...tasks) {
-  return condition ? tasks : [emptyGulpTask];
+  let finalTasks = [];
+
+  tasks.forEach(task => {
+    if (Array.isArray(task)) {
+      finalTasks = finalTasks.concat(task);
+    } else {
+      finalTasks.push(task);
+    }
+  });
+
+  const containsOnlyStringsAndFunctions = finalTasks.every(
+    task => typeof task === "string" || typeof task === "function"
+  );
+
+  if (!containsOnlyStringsAndFunctions) {
+    throw new Error("ArgumentException!, Please provide only");
+  }
+
+  const result = condition ? finalTasks : [emptyGulpTask];
+
+  return result;
 }
 
 module.exports = gulpTaskIf;
--- test.js
+++ test.js
@@ -1,11 +1,101 @@
-const test = require("ava");
 const gulpTaskIf = require("./index.js");
 
-test("foo", t => {
-  const input = "my-gulp-task";
-  const result = gulpTaskIf(true, [input]);
+describe("Accepts", () => {
+  it("strings", () => {
+    const task1 = "clean";
+    const task2 = "build";
+    const result = gulpTaskIf(true, task1, task2);
 
-  
+    expect(result[0]).toBe(task1);
+    expect(result[1]).toBe(task2);
+  });
 
-  t.is(result[0], input);
+  it("array", () => {
+    const task1 = "clean";
+    const tasks = ["build", "copy"];
+    const result = gulpTaskIf(true, task1, tasks);
+
+    expect(result[0]).toBe(task1);
+    expect(result[1]).toBe(tasks[0]);
+    expect(result[2]).toBe(tasks[1]);
+  });
+
+  it("functions", () => {
+    const inlineTask = function() {};
+
+    const task1 = "clean";
+    const tasks = ["build", "copy", inlineTask];
+    const result = gulpTaskIf(true, inlineTask, task1, tasks, inlineTask);
+
+    expect(typeof result[0] === "function").toBe(true);
+  });
+
+  it("arrow functions", () => {
+    const inlineTask = () => {};
+
+    const task1 = "clean";
+    const tasks = ["build", "copy", inlineTask];
+    const result = gulpTaskIf(true, inlineTask, task1, tasks, inlineTask);
+
+    expect(typeof result[0] === "function").toBe(true);
+  });
+});
+
+describe("Condition Handling", () => {
+  it("false - returns [Function] and calles provided callback", () => {
+    let called = false;
+    const asyncDone = () => (called = true);
+
+    const emptyTask = gulpTaskIf(false, "task1", "task2")[0];
+    emptyTask(asyncDone);
+    
+    expect(called).toBe(true);
+  });
+});
+
+describe("Throws", () => {
+  it("on with double nested arrays", () => {
+    const task1 = "clean";
+    const tasks = ["double nested!", ["build", "copy"]];
+
+    expect(() => {
+      gulpTaskIf(true, task1, tasks);
+    }).toThrow();
+  });
+
+  it("on objects", () => {
+    const task1 = {};
+    const tasks = ["double nested!", ["build", "copy"]];
+
+    expect(() => {
+      gulpTaskIf(true, task1, tasks);
+    }).toThrow();
+  });
+
+  it("on numbers", () => {
+    const task1 = 2;
+    const tasks = ["double nested!", ["build", "copy"]];
+
+    expect(() => {
+      gulpTaskIf(true, task1, tasks);
+    }).toThrow();
+  });
+
+  it("on null", () => {
+    const task1 = null;
+    const tasks = ["double nested!", ["build", "copy"]];
+
+    expect(() => {
+      gulpTaskIf(true, task1, tasks);
+    }).toThrow();
+  });
+
+  it("on undefined", () => {
+    const task1 = undefined;
+    const tasks = ["double nested!", ["build", "copy"]];
+
+    expect(() => {
+      gulpTaskIf(true, task1, tasks);
+    }).toThrow();
+  });
 });
